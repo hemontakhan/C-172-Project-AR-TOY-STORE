0637517fe1a724274ec0f44f5e4e5006d8f7f4a9
hemontakhan <76201341+hemontakhan@users.noreply.github.com>
2022-07-14 13:06:41 +0530

    Add files via upload

---
+++ js/addButtons.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent('store-buttons',{
+init: function(){ 
+    var orderButton = document.createElement('button');
+    orderButton.innerHTML = 'ORDER BUTTON';
+    orderButton.setAttribute('id','order-button');
+    orderButton.setAttribute('class','btn btn-warning');
+
+    var summaryButton = document.createElement('button');
+    summaryButton.innerHTML = 'SUMMARY BUTTON';
+    summaryButton.setAttribute('id','summary-button');
+    summaryButton.setAttribute('class','btn btn-warning');
+
+    var buttonDiv = document.getElementById('button-div');
+    buttonDiv.appendChild(orderButton);
+    buttonDiv.appendChild(summaryButton);
+}
+})
